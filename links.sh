807ed08dbc3a22132bac578a7113b2c570fbbdbd
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-09-11 16:11:43 -0300

    auto

---
+++ links.sh
@@ -0,0 +1,7 @@
+ln -sf .xinitrc ~/.xinitrc
+ln -sf .bashrc ~/.bashrc
+ln -sf .fehbg ~/.fehbg
+ln -sf bg.jpg ~/bg.jpg
+
+ln -sf .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf .config/picom.conf ~/.config/picom.conf
