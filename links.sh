e27d86c4cf5dfad4b237cf174b4eeff4ac9c6f55
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-09-11 16:21:34 -0300

    auto

--- links.sh
+++ links.sh
@@ -1,7 +1,9 @@
+#home
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.fehbg ~/.fehbg
 ln -sf ~/dotfiles/bg.jpg ~/bg.jpg
 
+#.config
 ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/.config/picom.conf ~/.config/picom.conf
