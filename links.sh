7b94209583c551a4f752f85e9ab2bf15bd7d449c
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-09-13 09:36:30 -0300

    fix emacs

--- links.sh
+++ links.sh
@@ -3,7 +3,7 @@ ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.fehbg ~/.fehbg
 ln -sf ~/dotfiles/bg.jpg ~/bg.jpg
-ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 
 #.config
 ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
