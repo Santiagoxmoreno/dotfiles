0dcee5f33ef868f8ce255e89c16eaa2e6dfb0635
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-09-13 10:21:19 -0300

    alacritty

--- links.sh
+++ links.sh
@@ -6,5 +6,6 @@ ln -sf ~/dotfiles/bg.jpg ~/bg.jpg
 ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 
 #.config
-ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+# ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/.config/picom.conf ~/.config/picom.conf
+ln -sf ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
