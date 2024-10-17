62138af0e9c19a2db4f736839032f8fad10f5b40
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2024-10-17 16:13:23 -0300

    removed emacs and kitty

--- links.sh
+++ links.sh
@@ -2,10 +2,8 @@
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.fehbg ~/.fehbg
-#ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 
 #.config
-ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/.config/picom.conf ~/.config/picom.conf
 ln -sf ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/dotfiles/.config/micro/colorschemes/sangl.micro ~/.config/micro/colorschemes/sangl.micro
