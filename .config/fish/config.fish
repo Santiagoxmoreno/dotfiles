ba8dffddbc36214638279e8afc0b578f5108f853
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-02-06 19:43:45 -0300

    changed shell to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+alias vi="nvim"
+alias vim="nvim"
+alias hyprcfg="micro ~/.config/hypr/hyprland.conf"
+alias dwmcfg="cd ~/dwm/dwm/; micro config.h"
+alias startvm="sudo virsh start win10"
+alias fixres="xrandr --output HDMI-0 --auto"
+alias copy="xclip -i -selection clipboard"
+alias play="mpv (xclip -o -selection clipboard)"
+alias ssh="env TERM=xterm-256color ssh"
+
+function gitpush
+    read -P "Commit name: " commitname
+    git add .
+    git commit -m "$commitname"
+    git push
+end
+
+function fixinput
+    setxkbmap -layout latam
+    xinput set-prop "Corsair Corsair Gaming HARPOON RGB Mouse" "libinput Accel Profile Enabled" 0 1 0
+    xinput set-prop "Corsair Corsair Gaming HARPOON RGB Mouse" "libinput Accel Speed" -0.75
+end
+
+set -x MICRO_TRUECOLOR 1
--- links.sh
+++ links.sh
@@ -1,7 +1,6 @@
 #home
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc.local ~/.zshrc.local
 ln -sf ~/dotfiles/.fehbg ~/.fehbg
 
 #.config
@@ -9,6 +8,7 @@ ln -sf ~/dotfiles/.config/picom.conf ~/.config/picom.conf
 ln -sf ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/dotfiles/.config/micro/colorschemes/sangl.micro ~/.config/micro/colorschemes/sangl.micro
 ln -sf ~/dotfiles/.config/feh/buttons ~/.config/feh/buttons
+ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 
 #vm hooks
 sudo ln -sf ~/dotfiles/hooks /etc/libvirt
