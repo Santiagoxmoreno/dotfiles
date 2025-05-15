b6b97e14a554c0a6800df94634cfd5c333967962
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-05-15 20:46:37 -0300

    adjusted sens

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,7 @@ end
 function fixinput
     setxkbmap -layout latam
     xinput set-prop "pointer:Logitech G305" "libinput Accel Profile Enabled" 0 1 0
-    xinput set-prop "pointer:Logitech G305" "libinput Accel Speed" -0.25
+    xinput set-prop "pointer:Logitech G305" "libinput Accel Speed" -0.50
 end
 
 set -x MICRO_TRUECOLOR 1
