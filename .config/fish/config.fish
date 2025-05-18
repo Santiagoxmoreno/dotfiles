48da187d53ac2bc7e7ea75ed52a31fc0f02d07f7
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-05-18 00:21:36 -0300

    fixed fixinput layout

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,7 +16,7 @@ function gitpush
 end
 
 function fixinput
-    setxkbmap -layout latam
+    setxkbmap -layout es
     xinput set-prop "pointer:Logitech G305" "libinput Accel Profile Enabled" 0 1 0
     xinput set-prop "pointer:Logitech G305" "libinput Accel Speed" -0.50
 end
